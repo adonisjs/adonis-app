899a66980bb05a4efa7a77f21bb3eab4d6da99fd
eNzy <eNzyOfficial@users.noreply.github.com>
2018-04-13 19:43:54 +0100

    Update app.js

--- config/app.js
+++ config/app.js
@@ -19,19 +19,6 @@ module.exports = {
 
   /*
   |--------------------------------------------------------------------------
-  | Application Environment
-  |--------------------------------------------------------------------------
-  |
-  | This value determines the "environment" your application is currently
-  | running in. This may determine how you prefer to configure various
-  | services your application utilizes. Set this in your ".env" file.
-  |
-  */
-
-  env: Env.get('NODE_ENV', 'production'),
-
-  /*
-  |--------------------------------------------------------------------------
   | App Key
   |--------------------------------------------------------------------------
   |
