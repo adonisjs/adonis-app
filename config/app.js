5f1782b2be842d154990f0419c7664bb5abe556f
eNzy <eNzyOfficial@users.noreply.github.com>
2018-04-13 20:44:10 +0100

    Update app.js

--- config/app.js
+++ config/app.js
@@ -9,9 +9,9 @@ module.exports = {
   | Application Name
   |--------------------------------------------------------------------------
   |
-  | This value is the name of your application. This value is used when the
-  | framework needs to place the application's name in a notification or
-  | any other location as required by the application or its packages.
+  | This value is the name of your application and can used when you
+  | need to place the application's name in a email, view or
+  | other location.
   |
   */
 
