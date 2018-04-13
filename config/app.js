a444bea297f8eacf068b31dbc51343c8c44a283f
eNzy <eNzyOfficial@users.noreply.github.com>
2018-04-13 18:01:40 +0100

    Added name and env to app.js

--- config/app.js
+++ config/app.js
@@ -3,6 +3,32 @@
 const Env = use('Env')
 
 module.exports = {
+  
+  /*
+  |--------------------------------------------------------------------------
+  | Application Name
+  |--------------------------------------------------------------------------
+  |
+  | This value is the name of your application. This value is used when the
+  | framework needs to place the application's name in a notification or
+  | any other location as required by the application or its packages.
+  |
+  */
+
+  name: Env.get('APP_NAME', 'AdonisJs'),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Application Environment
+  |--------------------------------------------------------------------------
+  |
+  | This value determines the "environment" your application is currently
+  | running in. This may determine how you prefer to configure various
+  | services your application utilizes. Set this in your ".env" file.
+  |
+  */
+
+  env: Env.get('NODE_ENV', 'production'),
 
   /*
   |--------------------------------------------------------------------------
